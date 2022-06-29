c195088009744fa63a27b535b457ab43f15cd53c
arish khan <mohammadarishkhan503@gmail.com>
2022-06-29 18:04:40 +0530

    updates

---
+++ Sanke $ Leader/Scene/Home/Cell/HomeTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HomeTableViewCell.swift
+//  Sanke $ Leader
+//
+//  Created by Arish Khan on 17/03/22.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
